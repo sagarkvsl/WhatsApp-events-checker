bb2863a94869f5df75caea2cc60eb31764b5bb4b
CodeSandbox <support@codesandbox.io>
2023-10-29 07:18:54 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import requests
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route('/get_api_response', methods=['POST'])
+def get_api_response():
+    api_key = request.form['api_key']
+    limit = request.form['limit']
 
+    url = f'https://api.brevo.com/v3/whatsapp/statistics/events?limit={limit}&offset=0&sort=desc'
 
-if __name__ == "__main__":
-    app.run()
+    headers = {
+        'api-key': api_key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return render_template('response.html', data=data)
+    else:
+        return 'Error: Unable to fetch data from the API'
+
+if __name__ == '__main__':
+    app.run(debug=True)
